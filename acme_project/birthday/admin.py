0762f44a8f740cd5c0fcb4e34b56aa3175079939
PetrMyln <mylnikovpetr@mail.ru>
2024-10-01 12:06:44 +0300

    Валидация формы

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+admin.site.empty_value_display = 'Не задано'
+
+
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'birthday',
+    )
+
+
+
+admin.site.register(Birthday, BirthdayAdmin)
