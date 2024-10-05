de6a6c519e8cbc09cdae118efe2815637d76ba11
PetrMyln <mylnikovpetr@mail.ru>
2024-10-05 11:09:08 +0300

    CBV congratulation. add tag model error auth/registration

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -7,6 +7,8 @@ from .validators import real_age
 
 User = get_user_model()
 
+class Tag(models.Model):
+    tag = models.CharField('Тег', max_length=20)
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
@@ -18,6 +20,12 @@ class Birthday(models.Model):
     author = models.ForeignKey(
         get_user_model(), verbose_name='Автор записи', on_delete=models.CASCADE, null=True, blank=True,
         related_name='birthdays'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        blank=True,
+        help_text='Удерживайте Ctrl для выбора нескольких вариантов'
     )
 
     objects = models.Manager()
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,10 +2,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 from .forms import BirthdayForm
-from .models import Birthday
+from .models import Birthday, Congratulation
 from .utils import calculate_birthday_countdown
 
 from django.contrib.auth.decorators import login_required
@@ -68,20 +68,22 @@ def simple_view(request):
 
 
 
-@login_required
-def add_comment(request, pk):
-    # Получаем объект дня рождения или выбрасываем 404 ошибку.
-    birthday = get_object_or_404(Birthday, pk=pk)
-    # Функция должна обрабатывать только POST-запросы.
-    form = CongratulationForm(request.POST)
-    if form.is_valid():
-        # Создаём объект поздравления, но не сохраняем его в БД.
-        congratulation = form.save(commit=False)
-        # В поле author передаём объект автора поздравления.
-        congratulation.author = request.user
-        # В поле birthday передаём объект дня рождения.
-        congratulation.birthday = birthday
-        # Сохраняем объект в БД.
-        congratulation.save()
-    # Перенаправляем пользователя назад, на страницу дня рождения.
-    return redirect('birthday:detail', pk=pk)
+class CongratulationCreateView(LoginRequiredMixin, CreateView):
+    birthday = None
+    model = Congratulation
+    form_class = CongratulationForm
+
+    # Переопределяем dispatch()
+    def dispatch(self, request, *args, **kwargs):
+        self.birthday = get_object_or_404(Birthday, pk=kwargs['pk'])
+        return super().dispatch(request, *args, **kwargs)
+
+    # Переопределяем form_valid()
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.birthday = self.birthday
+        return super().form_valid(form)
+
+    # Переопределяем get_success_url()
+    def get_success_url(self):
+        return reverse('birthday:detail', kwargs={'pk': self.birthday.pk})
