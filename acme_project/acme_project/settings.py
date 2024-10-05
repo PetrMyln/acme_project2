598e17fc1e3d0c2f6999d8d98b712ed3ee0ad4e9
PetrMyln <mylnikovpetr@mail.ru>
2024-10-05 10:41:40 +0300

    add app core. dont fix auth/registration

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -4,9 +4,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-m&$lzdzkutvrbr5vt=jpm)7#g7cken_tk%($ty+w902n7wb#=e'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    # Когда проект будет опубликован и станет доступен для пользователей,
+    # в этот список нужно будет добавить и адреса домена, где он будет размещён,
+    # например 'acme.not' и 'www.acme.not'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -18,6 +24,7 @@ INSTALLED_APPS = [
     'birthday.apps.BirthdayConfig',
     'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
+    'core.apps.CoreConfig',
     'django_bootstrap5',
     'django_extensions',
 ]
@@ -102,3 +109,5 @@ AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'pages:homepage'
 
 LOGIN_URL = 'login'
+
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
